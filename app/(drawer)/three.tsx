2e20fe623f4d80b1ba3ee48af195d65bbd308e4e
koolm4nn <mar_kuhlmann@protonmail.com>
2025-09-18 20:46:24 +1200

    Testing action sheet [skip ci]

--- app/(drawer)/three.tsx
+++ app/(drawer)/three.tsx
@@ -1,31 +1,41 @@
-import { StyleSheet } from 'react-native';
+// Using the provided hook
+import { useActionSheet } from '@expo/react-native-action-sheet';
+import { Button, View } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function Three() {
+  const { showActionSheetWithOptions } = useActionSheet();
 
-export default function TabThreeScreen() {
+  const onPress = () => {
+    const options = ['Delete', 'Save', 'Cancel'];
+    const destructiveButtonIndex = 0;
+    const cancelButtonIndex = 2;
+
+    showActionSheetWithOptions({
+      options,
+      cancelButtonIndex,
+      destructiveButtonIndex
+    }, (selectedIndex) => {
+      switch (selectedIndex) {
+        case 1:
+          console.log("Save");
+          // Save
+          break;
+
+        case destructiveButtonIndex:
+          console.log("Delete");
+          // Delete
+          break;
+
+        case cancelButtonIndex:
+          console.log("Cancel");
+          // Canceled
+      }});
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
-    </View>
-  );
-}
+    <View className='p-20'>
+      <Button title="Menu" onPress={onPress}/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 2,
-    width: '80%',
-  },
-});
+    </View>
+  )
+};
